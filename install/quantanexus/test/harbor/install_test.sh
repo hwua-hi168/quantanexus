24126bfd5cc0b82ab782067063c7029c33f7e484
david.cheng <david.cheng@com-kube-ovn-master-1>
2025-10-09 15:05:39 +0000

    cert-manager commit

--- install/quantanexus/test/harbor/install_test.sh
+++ install/quantanexus/test/harbor/install_test.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
+# 添加官方仓库（如果未添加）
 helm repo add harbor https://helm.goharbor.io
+helm repo update
 
-helm upgrade --install harbor harbor/harbor -n harbor --create-namespace \
-  --set expose.type=nodePort \
-  --set externalURL=http://192.168.103.161:30002 \
-  --set expose.tls.enabled=false \
-  --set expose.nodePort.ports.http.port=80 \
-  --set expose.nodePort.ports.http.nodePort=32600 \
-  --set expose.nodePort.ports.https.port=443 \
-  --set expose.nodePort.ports.https.nodePort=30003 \
-  --set persistence.enabled=true \
-  --set persistence.persistentVolumeClaim.registry.storageClass=longhorn \
-  --set persistence.persistentVolumeClaim.registry.accessMode=ReadWriteOnce \
-  --set persistence.persistentVolumeClaim.registry.size=15Gi \
-  --set persistence.persistentVolumeClaim.jobservice.storageClass=longhorn \
-  --set persistence.persistentVolumeClaim.jobservice.accessMode=ReadWriteOnce \
-  --set persistence.persistentVolumeClaim.jobservice.size=10Gi \
-  --set persistence.persistentVolumeClaim.database.storageClass=longhorn \
-  --set persistence.persistentVolumeClaim.database.accessMode=ReadWriteOnce \
-  --set persistence.persistentVolumeClaim.database.size=10Gi \
-  --set persistence.persistentVolumeClaim.redis.storageClass=longhorn \
-  --set persistence.persistentVolumeClaim.redis.accessMode=ReadWriteOnce \
-  --set persistence.persistentVolumeClaim.redis.size=5Gi \
-  --set database.internal.initContainer.permissions.enabled=true \
-  --set harborAdminPassword=admin
+# 安装Harbor，同时应用自定义Job和配置
+helm upgrade --install harbor harbor/harbor \
+  --namespace harbor \
+  --create-namespace \
+  -f values-test.yaml 
