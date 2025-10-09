0938b1d5403a046d1406c85e247ae9a7a7170b1c
david.cheng <david.cheng@com-kube-ovn-master-1>
2025-10-09 16:02:09 +0000

    gpu-operator

--- install/quantanexus/test/gpu-operator/install-test.sh
+++ install/quantanexus/test/gpu-operator/install-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia 
 helm repo update
-helm upgrade --install --wait gpu-operator \
+helm upgrade --install --wait gpu-operator ./gpu-operator-v25.3.4.tgz \
      -n gpu-operator --create-namespace \
-     nvidia/gpu-operator
+     -f values-test.yaml \
+     --wait --timeout=15m
 
 
 # kubectl label nodes $NODE nvidia.com/gpu.deploy.operands=false   
