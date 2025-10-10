6a3ae67c21ed6d736adf2d6088833c6c6af91369
david.cheng <david.cheng@com-kube-ovn-master-1>
2025-10-10 03:11:47 +0000

    ingress

--- install/quantanexus/test/ingress-nginx/install-test.sh
+++ install/quantanexus/test/ingress-nginx/install-test.sh
@@ -1,2 +1,5 @@
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo add ingress-nginx https://helm.hi168.com/charts/
 helm repo update
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+  -n ingress-nginx --create-namespace \
+  -f values-test.yaml
