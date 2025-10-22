a2ad213f17b34c92022b0785d47fb2659ee4296f
tianbao <xie.tianbao44@gmail.com>
2025-10-22 13:55:31 +0800

    update

--- install/test/helm/network/calico/install-test.sh
+++ install/test/helm/network/calico/install-test.sh
@@ -8,4 +8,4 @@ helm upgrade --install calico-tigera-operator hi168/tigera-operator  \
   --namespace kube-system \
   --set bpf=true \
   --set mtu="1450" \  # eBPF模式推荐MTU
-  --set calicoNetworkBackend="bpffs"
+  --set calicoNetworkBackend="bpffs" -f values-calico.yaml
