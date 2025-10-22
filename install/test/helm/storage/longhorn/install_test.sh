71cf47e4fc51229b37ccd9b92896a974abc96c3a
tianbao <xie.tianbao44@gmail.com>
2025-10-22 12:28:37 +0800

    update-longhorn

--- install/test/helm/storage/longhorn/install_test.sh
+++ install/test/helm/storage/longhorn/install_test.sh
@@ -4,5 +4,5 @@
 helm repo add hi168 https://hi168.com/charts
 
 helm install longhorn hi168/longhorn --namespace longhorn-system --create-namespace --version 1.10.0 \
-    -f values-test.yaml 
+    -f values-longhorn.yaml
     
