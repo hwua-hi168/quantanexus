e01180bd0999f273e619d1ecd18b31ab1298747f
david.cheng <david.cheng@com-kube-ovn-master-1>
2025-10-30 16:10:13 +0000

    correct the mistake

--- install/test/helm/ingress-nginx/install-test.sh
+++ install/test/helm/ingress-nginx/install-test.sh
@@ -1,6 +1,7 @@
-helm repo add hi168 https://hi168.com/charts 
+helm repo add hi168 https://hi168.com/charts  2>/dev/null
 helm repo update
 
-helm upgrade --install ingress-nginx hi168/ingress-nginx --version 4.0.18 ingress-nginx \
+helm upgrade --install ingress-nginx hi168/ingress-nginx --version 4.0.18  \
   -n ingress-nginx --create-namespace \
+  --set defaultSettings.volumeEncryption=false \
   -f values.yaml 
