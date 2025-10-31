44a1fa9a413ac938a7cc95ee9181b9755910d734
tianbao <xie.tianbao44@gmail.com>
2025-10-31 09:56:53 +0800

    bug-fix

--- install/test/helm/ingress-nginx/install-test.sh
+++ install/test/helm/ingress-nginx/install-test.sh
@@ -1,6 +1,6 @@
 helm repo add hi168 https://hi168.com/charts 
 helm repo update
 
-helm upgrade --install ingress-nginx hi168/ingress-nginx --version 4.0.18 ingress-nginx \
+helm upgrade --install ingress-nginx hi168/ingress-nginx --version 4.0.18 \
   -n ingress-nginx --create-namespace \
-  -f values.yaml 
+  -f values.yaml
