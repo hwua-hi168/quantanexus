d08ec4f1d87715d26d751774635829fc6310a10d
tianbao <xie.tianbao44@gmail.com>
2025-11-18 11:32:06 +0800

    update  image for juicefs-csi-driver

--- install/test/helm/redis/install-sentinel.sh
+++ install/test/helm/redis/install-sentinel.sh
@@ -8,6 +8,7 @@ helm repo update
 # 3. 安装 redis to kube-system 
 helm upgrade -i redis-sentinel bitnami/redis \
   --namespace kube-system --create-namespace \
+  --version 23.2.12 \
   -f values-sentinel.yaml 
 
 # 4. 观察
--- install/test/helm/storage/juicefs/install.sh
+++ install/test/helm/storage/juicefs/install.sh
@@ -1,3 +1,6 @@
+helm repo add juicefs https://juicedata.github.io/charts
+helm repo update
+
 helm upgrade --install juicefs-csi-driver juicefs/juicefs-csi-driver \
     -n juicefs \
     --create-namespace -f values.yaml
